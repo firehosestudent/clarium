ca6127507d4d96efe33452b9c7d27bacdf38bb1b
jonny phun <jonny_phun@hotmail.com>
2019-10-03 21:37:47 +0000

    test for index action

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,6 @@
+class ListingsController < ApplicationController
+
+  def index
+  end
+
+end
---
+++ spec/controllers/listings_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe ListingsController, type: :controller do
+
+  describe "grams#index action" do 
+
+    it "should successfully show the page" do 
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+end
